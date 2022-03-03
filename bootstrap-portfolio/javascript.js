e1b1522c2b34197a8cfd35764e18c08c570fcdf6
Saddan Syah Akbar <saddanakbar@gmail.com>
2022-03-03 21:04:38 +0700

    update

---
+++ bootstrap-portfolio/javascript.js
@@ -0,0 +1,28 @@
+// var i = 0;
+// while(i < 10){
+//     console.log(`Angkot No.${i+1} berjalan dengan baik`);
+//     i++;
+// }
+
+// function tambah(){
+//     var hasil = 0;
+//     for(var i = 0; i < arguments.length; i++){
+//         hasil += arguments[i];
+//     }
+
+//     return hasil;
+// }
+
+// console.log(tambah(1,2,3,4,5,6,7,8,9));
+
+var penumpang = []
+var tambahPenumpang = function(namaPenumpang, penumpang){
+    if(penumpang.length == 0){
+        penumpang.push(namaPenumpang)
+    } 
+    else {
+
+    }
+}
+
+
